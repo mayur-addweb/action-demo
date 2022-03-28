eb8d32a24d1270eb0981e35bb16f89f2c7161197
mayur-addweb <mayur@addwebsolution.in>
2022-03-28 17:02:23 +0530

    action

--- wp-config.php
+++ wp-config.php
@@ -12,10 +12,11 @@ require $root_dir . '/environments/dotenv.php';
 /**
  * Use Dotenv to set required environment variables and load .env file in root
  */
-$dotenv = dotenv( $root_dir );
-$dotenv->load();
-$dotenv->required( [ 'DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL' ] );
-
+#$dotenv = dotenv( $root_dir );
+$dotenv->load()
+$dotenv->require [ 'DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL' ] );
+efcrev 
+erfc
 /**
  * Set up our global environment constant and load its config first
  * Default: production
